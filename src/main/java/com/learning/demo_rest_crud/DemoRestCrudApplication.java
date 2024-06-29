e0d9841a8022b770a60f34f6b45ee571e67b67ae
kiiran09 <kirankardile23@gmail.com>
2024-06-29 09:14:36 +0530

    Exception handling

---
+++ src/main/java/com/learning/demo_rest_crud/DemoRestCrudApplication.java
@@ -0,0 +1,13 @@
+package com.learning.demo_rest_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestCrudApplication.class, args);
+	}
+
+}
